51be94ae1d17b29f95cc8be97b8ed7f1ba85fea4
jake grella <jake@jakegrella.com>
2022-04-18 08:57:48 -0500

    add contact card

--- src/cardInfo.js
+++ src/cardInfo.js
@@ -1,24 +1,31 @@
 const cardInfo = [
   {
-    title: 'real estate photography',
-    description:
-      'we specialize in capturing interior and exterior residential homes',
+    title: 'contact us',
+    description: 'email: support@headedwesttt.com social: @headedwesttt',
     photoUrl:
-      'https://images.unsplash.com/photo-1639060631449-4a3ba54a203a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
+      'https://images.unsplash.com/photo-1638115331857-4ef2e0ed2cde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     photoAlt: '',
   },
   {
-    title: 'film development',
-    description: 'have your your 35mm color film developed with us in house',
+    title: 'project antithetical',
+    description: "we're building an app and will share more soon",
+    photoUrl:
+      'https://images.unsplash.com/photo-1638570507495-2fbca805f60b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+    photoAlt: '',
+  },
+  {
+    title: 'photo services',
+    description: '35mm film development, editing, and more',
     photoUrl:
       'https://images.unsplash.com/photo-1642098043424-5eb2f9f1135d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
     photoAlt: '',
   },
   {
-    title: 'drone videography',
-    description: 'let us capture your property from above',
+    title: 'real estate photography',
+    description:
+      'interior, exterior, and drone photos of residential homes and properties',
     photoUrl:
-      'https://images.unsplash.com/photo-1638570507495-2fbca805f60b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+      'https://images.unsplash.com/photo-1639060631449-4a3ba54a203a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
     photoAlt: '',
   },
 ]
