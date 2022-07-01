65f6edd216267797274ba28234b0e75b52fc0b9c
jake grella <jake@jakegrella.com>
2022-07-01 14:55:11 -0500

    update card images

--- src/cardInfo.js
+++ src/cardInfo.js
@@ -3,14 +3,14 @@ const cardInfo = [
     title: 'Contact Us',
     description: 'email: support@headedwesttt.com social: @headedwesttt',
     photoUrl:
-      'https://images.unsplash.com/photo-1638115331857-4ef2e0ed2cde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      'https://images.unsplash.com/photo-1656705123889-b63b6c9d1d65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
     photoAlt: '',
   },
   {
     title: 'Westtt',
     description: 'Real estate and architecture content services',
     photoUrl:
-      'https://images.unsplash.com/photo-1638570507495-2fbca805f60b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+      'https://images.unsplash.com/photo-1656704903724-f49b0233eaa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     photoAlt: '',
   },
 ]
